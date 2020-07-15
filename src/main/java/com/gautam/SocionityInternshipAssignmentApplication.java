8bb659b1fde115413b42dd04206b2d7521d91813
gtmb007 <gtmb007@gmail.com>
2020-07-15 10:14:16 +0530

    First commit

---
+++ src/main/java/com/gautam/SocionityInternshipAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.gautam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocionityInternshipAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocionityInternshipAssignmentApplication.class, args);
+	}
+
+}
